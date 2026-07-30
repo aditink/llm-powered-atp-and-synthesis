05ef7667f4e3c0a62978a6de514dc1c4ff857a27
aditink <aditi.n.kabra@gmail.com>
2026-07-29 22:11:29 -0400

    Shorten repository and Pages path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/llm-powered-automatic-theorem-proving-and-synthesis-for-hybrid-systems-and-games/",
+  base: "/llm-powered-atp-and-synthesis/",
   build: {
     target: "esnext",
   },
